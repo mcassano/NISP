029b764f8f901aa023cb61ed9b57045eaac277f4
Michael Cassano <mcassano@gmail.com>
2015-06-04 15:18:10 -0600

    mcassano: add output for PRG ROM and CHR ROM in bytes if magic passes

--- nisp.scm
+++ nisp.scm
@@ -8,21 +8,40 @@
 ;     
 
 (define romfile "smb.nes") ; Super Mario Bros. (Japan, USA).nes
- 
+(define prgrom-size -1)
+(define chrrom-size -1)
+
 (define check-magic-header
   (lambda (char num)
     (cond ((and (= num 0) (eq? char (integer->char #x4e))) #t) ; N
 	  ((and (= num 1) (eq? char (integer->char #x45))) #t) ; E
 	  ((and (= num 2) (eq? char (integer->char #x53))) #t) ; S
+	  ((and (= num 3) (eq? char (integer->char #x1a))) #t) ; MS-DOS EOF
 	  (else #f))))
+
+(define parse-header
+  (lambda (rom-port)
+    ; Assuming first 4 bytes of 16 have been consumed
+    (begin
+      (set! prgrom-size (* (char->integer (read-char rom-port)) 16384))
+      (set! chrrom-size (* (char->integer (read-char rom-port)) 8192))
+      (display (string "PRG ROM size " prgrom-size " bytes.\n"))
+      (display (string "CHR ROM size " chrrom-size " bytes.\n"))
+      )
+    ))
 
 (define run
   (lambda () 
     (begin
+      (newline)
       (define rom-port (open-binary-input-file romfile))
-      (if (and (check-magic-header (read-char rom-port) 0) (check-magic-header (read-char rom-port) 1) (check-magic-header (read-char rom-port) 2))
-	  #t ; Header magic passed
-	  #f ; Header magic didn't pass
+      ; Check magic header for #\NES<EOF>
+      (if (and (check-magic-header (read-char rom-port) 0) 
+	       (check-magic-header (read-char rom-port) 1) 
+	       (check-magic-header (read-char rom-port) 2) 
+	       (check-magic-header (read-char rom-port) 3))
+	  (parse-header rom-port)
+	  (display "ROM check failed magic.\n")
 	  )
       )
     ))
