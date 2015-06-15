c42e393ff7a3720b823b2b933fe44f0001256632
Michael Cassano <mcassano@gmail.com>
2015-06-14 20:20:24 -0600

    mcassano: implement Y register and a few more opcodes

--- nisp.scm
+++ nisp.scm
@@ -31,6 +31,7 @@
 
 (define reg-accumulator #f)
 (define reg-x #f)
+(define reg-y #f)
 (define reg-pc #f)
 (define reg-sp #f)
 
@@ -136,7 +137,6 @@
 	)
     ))
     
-
 (define inc-sp
   (lambda (num)
     (set! reg-pc (+ reg-pc num))
@@ -152,100 +152,100 @@
        ((eq? inst #x78) (begin (display "SEI Set Interrupt (78)") (set! flag-interrupt #t)))
        ((eq? inst #xd8) (begin (display "CLD Clear Decimal (d8)") (set! flag-decimal #f)))
        ((eq? inst #x9a) (begin (display "Transfer X to Stack ptr (9a)") (set! reg-sp reg-x)))
-       ((eq? inst #x18) (begin (display "Clear Carry (18)")))
-       ((eq? inst #x38) (begin (display "Set Carry (38)")))
+       ((eq? inst #x18) (begin (display "*Clear Carry (18)")))
+       ((eq? inst #x38) (begin (display "*Set Carry (38)")))
        
 					; Branch Instructions
-       ((eq? inst #x10) (begin (display "Branch on Plus (10) ") (display1)(inc-sp 1)))
-       ((eq? inst #xb0) (begin (display "Branch on Carry Set (b0) ") (display1)(inc-sp 1)))
-       ((eq? inst #xd0) (begin (display "Branch on Not Equal (d0) ") (display1)(inc-sp 1)))
-       ((eq? inst #xf0) (begin (display "Branch on Equal (f0) ") (display1)(inc-sp 1)))
+       ((eq? inst #x10) (begin (display "*Branch on Plus (10) ") (display1)(inc-sp 1)))
+       ((eq? inst #xb0) (begin (display "*Branch on Carry Set (b0) ") (display1)(inc-sp 1)))
+       ((eq? inst #xd0) (begin (display "*Branch on Not Equal (d0) ") (display1)(inc-sp 1)))
+       ((eq? inst #xf0) (begin (display "*Branch on Equal (f0) ") (display1)(inc-sp 1)))
        
 					; ROR (ROtate Right)
-       ((eq? inst #x7e) (begin (display "Rotate right Absolute,X (7e) ") (display2)(inc-sp 2)))
+       ((eq? inst #x7e) (begin (display "*Rotate right Absolute,X (7e) ") (display2)(inc-sp 2)))
 
 					; LDA (LoaD Accumulator)
-       ((eq? inst #xa9) (begin (display "Load Accumulator Immediate (a9) ") (set! reg-accumulator (vector-ref main-cpuram reg-pc)) (display (string "Acc: " reg-accumulator))(inc-sp 1)))
-       ((eq? inst #xa5) (begin (display "Load Accumulator Zero Page (a5) ") (display1)))
-       ((eq? inst #xb5) (begin (display "Load Accumulator Zero Page,X (b5) ") (display1)))
-       ((eq? inst #xad) (begin (display "Load Accumulator Absolute (ad) ") (set! reg-accumulator (vector-ref main-cpuram (get-memory-address)))(display2)(display (string "Acc: " reg-accumulator))(inc-sp 2)))
-       ((eq? inst #xbd) (begin (display "Load Accumulator Absolute,X (bd) ") (display2)))
-       
+       ((eq? inst #xa9) (begin (display "Load Accumulator Immediate (a9) ") (set! reg-accumulator (vector-ref main-cpuram reg-pc))(display1)(inc-sp 1)))
+       ((eq? inst #xa5) (begin (display "!Load Accumulator Zero Page (a5) ") (display1)))
+       ((eq? inst #xb5) (begin (display "!Load Accumulator Zero Page,X (b5) ") (display1)))
+       ((eq? inst #xad) (begin (display "Load Accumulator Absolute (ad) ") (set! reg-accumulator (vector-ref main-cpuram (get-memory-address)))(display2)(inc-sp 2)))
+       ((eq? inst #xbd) (begin (display "Load Accumulator Absolute,X (bd) ") (set! reg-accumulator (vector-ref main-cpuram (+ (get-memory-address) reg-x)))(display2)(inc-sp 2)))
+    
 					; STA (STore Accumulator)
        ((eq? inst #x8d) (begin (display "Store accumulator Absolute (8d) ")  (vector-set! main-cpuram (get-memory-address) reg-accumulator)(inc-sp 2)))
-       ((eq? inst #x85) (begin (display "Store accumulator Zero Page (85) ") (display1)))
-       ((eq? inst #x9d) (begin (display "Store accumulator Absolute,X (9d) ") (display2)))
+       ((eq? inst #x85) (begin (display "!Store accumulator Zero Page (85) ") (display1)))
+       ((eq? inst #x9d) (begin (display "!Store accumulator Absolute,X (9d) ") (display2)))
        
 					; LDX (LoaD X register)
        ((eq? inst #xa2) (begin (display "Load X register Immediate (a2) ") (set! reg-x (vector-ref main-cpuram reg-pc)) (display1)(inc-sp 1)))
-       ((eq? inst #xbe) (begin (display "Load X register Absolute,Y (be) ") (display2)))
-       ((eq? inst #xae) (begin (display "Load X register Absolute (ae) ") (display2)))
+       ((eq? inst #xbe) (begin (display "!Load X register Absolute,Y (be) ") (display2)))
+       ((eq? inst #xae) (begin (display "!Load X register Absolute (ae) ") (display2)))
 
 					; RTS (ReTurn from Subroutine)
-       ((eq? inst #x60) (begin (display "Return from subroutine Implied (60)")))
+       ((eq? inst #x60) (begin (display "!Return from subroutine Implied (60)")))
 
 					; LDY (LoaD Y register)
-       ((eq? inst #xa0) (begin (display "Load Y register Immediate (a0) ") (display1)))
-       ((eq? inst #xac) (begin (display "Load Y register Absolute (ac) ") (display2)))
+       ((eq? inst #xa0) (begin (display "Load Y register Immediate (a0) ") (set! reg-y (vector-ref main-cpuram reg-pc))(display1)(inc-sp 1)))
+       ((eq? inst #xac) (begin (display "!Load Y register Absolute (ac) ") (display2)))
        
 					; CMP (CoMPare accumulator)
-       ((eq? inst #xc9) (begin (display "Compare accumulator (c9) ") (display1)))
+       ((eq? inst #xc9) (begin (display "*Compare accumulator (c9) ") (display1)(inc-sp 1)))
        
 					; CPX (ComPare X register)
-       ((eq? inst #xec) (begin (display "Compare X register Absolute (ec) ") (display2)))
-       ((eq? inst #xe0) (begin (display "Compare X register Immediate (e0) ") (display1)))
+       ((eq? inst #xec) (begin (display "!Compare X register Absolute (ec) ") (display2)))
+       ((eq? inst #xe0) (begin (display "!Compare X register Immediate (e0) ") (display1)))
        
 					; JMP (JuMP)
-       ((eq? inst #x4c) (begin (display "Jump Absolute (4c) ") (display2)))
+       ((eq? inst #x4c) (begin (display "!Jump Absolute (4c) ") (display2)))
        
 					; AND (bitwise AND with accumulator)
-       ((eq? inst #x29) (begin (display "Bitwise AND with accumulator Immediate (29) ") (display1)))
+       ((eq? inst #x29) (begin (display "!Bitwise AND with accumulator Immediate (29) ") (display1)))
        
 					; ORA (bitwise OR with Accumulator)
-       ((eq? inst #x09) (begin (display "Bitwise OR with accumulator Immediate (09) ") (display1)))
-       ((eq? inst #x01) (begin (display "Bitwise OR with accumulator Indirect,X  (01) ") (display1)))
+       ((eq? inst #x09) (begin (display "!Bitwise OR with accumulator Immediate (09) ") (display1)))
+       ((eq? inst #x01) (begin (display "!Bitwise OR with accumulator Indirect,X  (01) ") (display1)))
        
 					; RTI (ReTurn from Interrupt)
-       ((eq? inst #x40) (begin (display "Return from interrupt Implied (40)")))
+       ((eq? inst #x40) (begin (display "!Return from interrupt Implied (40)")))
 
 					; EOR (bitwise Exclusive OR)
-       ((eq? inst #x41) (begin (display "Bitwise exclusive OR Indirect,X (41) ") (display1)))
-       ((eq? inst #x49) (begin (display "Bitwise exclusive OR Immediate (49) ") (display1)))
-       ((eq? inst #x45) (begin (display "Bitwise exclusive OR Zero Page (45) ") (display1)))
+       ((eq? inst #x41) (begin (display "!Bitwise exclusive OR Indirect,X (41) ") (display1)))
+       ((eq? inst #x49) (begin (display "!Bitwise exclusive OR Immediate (49) ") (display1)))
+       ((eq? inst #x45) (begin (display "!Bitwise exclusive OR Zero Page (45) ") (display1)))
        
 					; INC (INCrement memory)
-       ((eq? inst #xee) (begin (display "Increment memory Absolute (ee) ") (display2)))
-       ((eq? inst #xe6) (begin (display "Increment memory Zero Page (e6) ") (display1)))
+       ((eq? inst #xee) (begin (display "!Increment memory Absolute (ee) ") (display2)))
+       ((eq? inst #xe6) (begin (display "!Increment memory Zero Page (e6) ") (display1)))
        
 					; Register Instructions
-       ((eq? inst #xca) (begin (display "Decrement X (ca)")))
-       ((eq? inst #xc8) (begin (display "Increment Y (c8)")))
-       ((eq? inst #xa8) (begin (display "Transfer A to Y (a8)")))
-       ((eq? inst #x88) (begin (display "Decrement Y (88)")))
-       ((eq? inst #xe8) (begin (display "Increment X (e8)")))
+       ((eq? inst #xca) (begin (display "*Decrement X (ca)")))
+       ((eq? inst #xc8) (begin (display "!Increment Y (c8)")))
+       ((eq? inst #xa8) (begin (display "!Transfer A to Y (a8)")))
+       ((eq? inst #x88) (begin (display "!Decrement Y (88)")))
+       ((eq? inst #xe8) (begin (display "!Increment X (e8)")))
 
 					; Stack Instructions
-       ((eq? inst #x68) (begin (display "Pull accumulator (68)")))
-       ((eq? inst #x48) (begin (display "Push accumulator (48)")))
+       ((eq? inst #x68) (begin (display "!Pull accumulator (68)")))
+       ((eq? inst #x48) (begin (display "!Push accumulator (48)")))
        
 					; JSR (Jump to SubRoutine)
-       ((eq? inst #x20) (begin (display "Jump to SubRoutine Absolute (20) ") (display2)))
+       ((eq? inst #x20) (begin (display "*Jump to SubRoutine Absolute (20) ") (display2)(inc-sp 2)))
 
 					; DEC (DECrement memory)
-       ((eq? inst #xde) (begin (display "Decrement memory Absolute,X (de) ") (display2)))
-       ((eq? inst #xce) (begin (display "Decrement memory Absolute (ce) ") (display2)))
+       ((eq? inst #xde) (begin (display "!Decrement memory Absolute,X (de) ") (display2)))
+       ((eq? inst #xce) (begin (display "!Decrement memory Absolute (ce) ") (display2)))
 
 					; STY (STore Y register)
-       ((eq? inst #x8c) (begin (display "Store Y register Absolute (8c) ") (display2)))
-       ((eq? inst #x84) (begin (display "Store Y register Zero Page (84) ") (display1)))
+       ((eq? inst #x8c) (begin (display "!Store Y register Absolute (8c) ") (display2)))
+       ((eq? inst #x84) (begin (display "!Store Y register Zero Page (84) ") (display1)))
        
 					; LSR (Logical Shift Right)
-       ((eq? inst #x4a) (begin (display "Logical shift right Accumulator (4a)")))
+       ((eq? inst #x4a) (begin (display "!Logical shift right Accumulator (4a)")))
 
        (else (display (string "*** Unknown instruction: " inst)))
        )
       (newline)
-      (if (< cntr 10)
+      (if (< cntr 50)
 	  (consume-instructions (+ cntr 1))
 	  )
       )
